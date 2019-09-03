fee2c0100330b10ab7d7030639e47d296ddc5292
unknown <raje1reddy@gmail.com>
2019-09-03 16:32:44 +0100

    code for string append test

--- src/test/java/com/padmaja/stringreplace/StringAppendTest.java
+++ src/test/java/com/padmaja/stringreplace/StringAppendTest.java
@@ -1,23 +1,27 @@
 package com.padmaja.stringreplace;
 
 import org.junit.Test;
-import java.lang.String;
+import java.lang.StringBuilder;
+
 
 import static org.junit.Assert.assertEquals;
 
-//import static com.padmaja.stringreplace.stringReplace.manipulateString;
-//import static org.junit.Assert.*;
 
- public class stringReplaceTest {
+
+ public class StringAppendTest {
 
      @Test
-     public void replaceTest1() {
-         stringReplace util = new stringReplace();
-         //manipulateString("HelloWorld");
-         String result = util.manipulateString("HelloWorld");
-         System.out.println(result);
-         String expectedresult = "vbppvzvbpz";
-         assertEquals(expectedresult,result);
+     public void appendTest1() {
+
+         StringBuilder sb1 =  new StringBuilder();
+         sb1.append("JAVA");
+         sb1=  sb1.append(new StringAppend("Oracle"));
+         System.out.println(sb1);
+         StringBuilder sb2 =  new StringBuilder();
+         System.out.println(sb2);
+        // String expectedresult = "JAVAcom.padmaja.stringreplace.StringAppend@e2144e4";
+        // String expectedresult = "JAVAcom.padmaja.stringreplace.StringAppend@2d928643";
+        //assertEquals(sb1,sb2);
      }
  }
 
